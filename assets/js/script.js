f33b65ebcdaa3d64e095fb789207eab7a1191cba
Jacob Studstill <jacob.studstill@gmail.com>
2023-01-12 11:45:30 -0500

    Worked on fetching data from the api to get back mininum data for correct flight

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,31 @@ function flightData() {
 	var dAirport = document.getElementById('depature-airport').value
 	var aAirport = document.getElementById('arrival-airport').value
 console.log(dDate,aDate,fNumber,dAirport,aAirport)
+travelData(dDate, aDate, fNumber, dAirport, aAirport)
 }
 
+function travelData (dDate, aDate, fNumber, dAirport, aAirport){
+	console.log(dDate)
+	console.log(aDate)
+	console.log(fNumber)
+	console.log(dAirport)
+	console.log(aAirport)
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6c1819b5d5msh224df4793f36756p19b1cfjsnef2c211706bb',
+		'X-RapidAPI-Host': 'flight-info-api.p.rapidapi.com'
+	}
+};
+
+fetch(`https://flight-info-api.p.rapidapi.com/schedules?version=v1&DepartureDate=${dDate}&ArrivalDate=${aDate}&FlightNumber=${fNumber}&DepartureAirport=${dAirport}&ArrivalAirport=${aAirport}`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+	
+
+
+
+
+}
 getDateBtn.addEventListener('click', flightData)
