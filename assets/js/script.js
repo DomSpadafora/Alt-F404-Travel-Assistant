561078b3cb8a4e99a935f9f632cc598ae9e67738
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-11 21:38:02 -0500

    add calender input on main page, still need to style

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+ 
+       ////Bulma Calender Functionality/////
+// To access to bulmaCalendar instance of an element
+var element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', function(datepicker) {
+		console.log(datepicker.data.value());
+	});
+}
